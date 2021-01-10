552cddbd100a0e4c854651b5cf1925416babbb44
lyo-n <1y0mmmm@gmail.com>
2021-01-10 18:32:58 +0200

    react-04_4

--- src/components/moreMovieInfo/moreMovieInfo.jsx
+++ src/components/moreMovieInfo/moreMovieInfo.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import propTypes from 'prop-types';
-// import styles from './Navigation.module.css';
 
 export default class MoreMovieInfo extends Component {
     render() {
@@ -29,39 +28,6 @@ export default class MoreMovieInfo extends Component {
         );
     }
 } 
-
-
-// render() {
-//     const { id } = this.props;
-//     return (
-//       <div>
-//         <p>Additional information</p>
-//         <NavLink
-//           exact
-//           to={{
-//             pathname: `/movie/${id}/cast`,
-//             state: { from: this.props.newLocation },
-//           }}
-//           className={styles.NavigationLink + ' ' + styles.AddInfoLink}
-//           activeClassName={styles.NavigationLinkActive}
-//         >
-//           Cast
-//         </NavLink>
-//         <br />
-//         <NavLink
-//           to={{
-//             pathname: `/movie/${id}/reviews`,
-//             state: { from: this.props.newLocation },
-//           }}
-//           className={styles.NavigationLink + ' ' + styles.AddInfoLink}
-//           activeClassName={styles.NavigationLinkActive}
-//         >
-//           Reviews
-//         </NavLink>
-//       </div>
-//     );
-//   }
-// }
 
 MoreMovieInfo.propTypes = {
     id: propTypes.number.isRequired,
